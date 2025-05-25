b3bed8b80f8faf97a02a845caab14a78234e6a1d
Kenneth Nunoo-Mensah <knunoo29@gmail.com>
2025-05-25 09:38:50 +0000

    Added landing page and initial signin and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Landing from "./routes/Landing";
+import {Routes, Route, Navigate} from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <h1 className="bg-purple-100">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/login" element={<login/>}/>
+        <Route path="/signup" element={<signup/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSMDvDelg-F7auzqvX6-zqZxDsdeIG4zE",
+  authDomain: "quick-notes-c4de9.firebaseapp.com",
+  projectId: "quick-notes-c4de9",
+  storageBucket: "quick-notes-c4de9.firebasestorage.app",
+  messagingSenderId: "865659133431",
+  appId: "1:865659133431:web:6b1b89147124936f30a2f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
